92886e9c8c3cffcb7aef6dfe5aae0789b3b0e477
Cyril <cpquitevis@gmail.com>
2017-10-21 13:26:01 +0800

    add build

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+__version__ = "1.0"
 from scan import scan
+from kivy.app import App
+from kivy.uix.label import Label
 
-print(scan(10))
+class Hello(App):
+    def build(self):
+        beacons = scan(10)
+        return Label(text=beacons[0])
+
+Hello().run()
